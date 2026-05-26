a64d4e3224b249f2e5d3723ca8a76dff5ae5d454
Julian Stecklina <julian.stecklina@cyberus-technology.de>
2026-05-26 10:41:13 +0200

    Initial import

---
+++ src/main.rs
@@ -0,0 +1,321 @@
+use std::str::FromStr;
+
+use anyhow::{Context, Result};
+use itertools::Itertools;
+use serde::Deserialize;
+use urlencoding::encode as url_encode;
+
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+struct JobSource {
+    r#ref: String,
+}
+
+impl JobSource {
+    fn branch(&self) -> Option<&str> {
+        self.r#ref.strip_prefix("refs/heads/")
+    }
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+struct Job {
+    index: i64,
+    derivation_status: String,
+
+    id: String,
+    source: JobSource,
+
+    // One of "OnPush" or "Config".
+    job_type: String,
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+struct JobList {
+    items: Vec<Job>,
+}
+
+impl IntoIterator for JobList {
+    type Item = Job;
+    type IntoIter = std::vec::IntoIter<Self::Item>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.items.into_iter()
+    }
+}
+
+#[derive(Deserialize, Debug)]
+enum AttributeValue {
+    #[serde(rename_all = "camelCase")]
+    Ok {
+        derivation_path: String,
+        status: String,
+    }, // What else?
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+struct Attribute {
+    path: Vec<String>,
+    value: AttributeValue,
+}
+
+impl Attribute {
+    /// Returns the name of the NixOS configuration if this attribute is a NixOS configuration.
+    fn nixos_configuration_name(&self) -> Option<String> {
+        (self.path.len() >= 2 && self.path[0] == "nixosConfigurations")
+            .then(|| self.path[1].clone())
+    }
+
+    fn derivation_path(&self) -> Option<String> {
+        match &self.value {
+            AttributeValue::Ok {
+                derivation_path, ..
+            } => Some(derivation_path.clone()),
+        }
+    }
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+struct Evaluation {
+    attributes: Vec<Attribute>,
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+struct Account {
+    id: String,
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+struct Output {
+    output_name: String,
+    output_path: String,
+}
+
+#[derive(Deserialize, Debug)]
+#[allow(dead_code)]
+enum Event {
+    Queued(serde_json::Value),
+    Started(serde_json::Value),
+    #[serde(rename_all = "camelCase")]
+    Built {
+        outputs: Vec<Output>,
+    },
+    Succeeded(serde_json::Value),
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+struct Derivation {
+    events: Vec<Vec<Event>>,
+}
+
+impl Derivation {
+    /// List all outputs of the derivation by scanning for `Built` events.
+    fn outputs(&self) -> impl Iterator<Item = &Output> {
+        self.events
+            .iter()
+            .flat_map(|events| events.iter())
+            .filter_map(|event| {
+                if let Event::Built { outputs } = event {
+                    Some(outputs.iter())
+                } else {
+                    None
+                }
+            })
+            .flatten()
+    }
+}
+
+/// The location of a project. Currently assumes GitHub.
+#[derive(clap::Parser, Clone, Debug)]
+struct ProjectLocation {
+    owner: String,
+    repo: String,
+    branch: String,
+}
+
+impl FromStr for ProjectLocation {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self> {
+        if let Some(location_str) = s.strip_prefix("github:") {
+            let components: Vec<&str> = location_str.split('/').collect();
+            if components.len() != 3 {
+                anyhow::bail!("Invalid GitHub URL: {}", s)
+            }
+            Ok(Self {
+                owner: components[0].to_owned(),
+                repo: components[1].to_owned(),
+                branch: components[2].to_owned(),
+            })
+        } else {
+            anyhow::bail!("Unsupported project location: {}", s)
+        }
+    }
+}
+
+#[derive(Debug, Clone, Default, clap::Subcommand)]
+enum Action {
+    /// Only print the discovered NixOS configuration toplevel.
+    #[default]
+    Discover,
+
+    /// Realise the NixOS configuration, but don't switch to it.
+    Realise,
+
+    /// Switch to the realised NixOS configuration.
+    Switch,
+
+    /// Switch to the realised NixOS configuration on the next boot.
+    Boot,
+}
+
+#[derive(Debug, clap::Parser)]
+struct Opts {
+    /// The location of the project to deploy. This has to be a string of the form `github:owner/repo/branch`.
+    #[clap(long)]
+    project: ProjectLocation,
+
+    /// The name of the NixOS configuration to deploy. If not specified, defaults to
+    /// the hostname.
+    #[clap(long)]
+    nixos_config: Option<String>,
+
+    /// Which command to perform.
+    #[clap(subcommand)]
+    action: Action,
+}
+
+fn main() -> Result<()> {
+    let opts = <Opts as clap::Parser>::parse();
+    let forge = "github";
+    let owner = &opts.project.owner;
+    let repo = &opts.project.repo;
+    let branch = &opts.project.branch;
+
+    let nixos_config_name: String = if let Some(config_name) = &opts.nixos_config {
+        config_name.to_owned()
+    } else {
+        hostname::get()
+            .context("Failed to query hostname")?
+            .to_str()
+            .context("Hostname is not valid UTF-8")?
+            .to_owned()
+    };
+    let accounts: Vec<Account> = ureq::get(format!(
+        "https://hercules-ci.com/api/v1/accounts?site={}&name={}",
+        url_encode(forge),
+        url_encode(owner)
+    ))
+    .call()
+    .context("Failed to fetch account")?
+    .body_mut()
+    .read_json()
+    .context("Failed to parse account")?;
+    let account = &accounts[0];
+
+    let jobs: JobList = ureq::get(format!(
+        "https://hercules-ci.com/api/v1/site/github/account/{}/project/{}/jobs?limit=25",
+        url_encode(owner),
+        url_encode(repo)
+    ))
+    .call()
+    .context("Failed to fetch jobs")?
+    .body_mut()
+    .read_json()
+    .context("Failed to parse jobs")?;
+
+    if let Some(latest_success) = jobs
+        .into_iter()
+        .sorted_by_key(|job| -job.index)
+        .find(|job| {
+            job.derivation_status == "Success"
+                && job.job_type == "OnPush"
+                && job.source.branch() == Some(branch)
+        })
+    {
+        let evaluation: Evaluation = ureq::get(&format!(
+            "https://hercules-ci.com/api/v1/jobs/{}/evaluation",
+            latest_success.id
+        ))
+        .call()
+        .context("Failed to fetch evaluation")?
+        .body_mut()
+        .read_json()
+        .context("Failed to parse evaluation")?;
+
+        for attr in &evaluation.attributes {
+            if let Some(config_name) = attr.nixos_configuration_name()
+                && let Some(derivation_path) = attr.derivation_path()
+            {
+                if config_name != nixos_config_name {
+                    continue;
+                }
+
+                let derivation: Derivation = ureq::get(&format!(
+                    "https://hercules-ci.com/api/v1/accounts/{}/derivations/{}",
+                    account.id,
+                    url_encode(&derivation_path)
+                ))
+                .call()
+                .context("Failed to fetch derivation")?
+                .body_mut()
+                .read_json()
+                .context("Failed to parse derivation")?;
+
+                if let Some(output) = derivation
+                    .outputs()
+                    .find(|output| output.output_name == "out")
+                {
+                    if let Action::Discover = &opts.action {
+                        println!("{}", output.output_path);
+                        break;
+                    }
+
+                    println!(
+                        "Realising toplevel for {}: {}",
+                        config_name, output.output_path
+                    );
+
+                    std::process::Command::new("nix-store")
+                        .arg("--realise")
+                        .arg(&output.output_path)
+                        .status()
+                        .context("Failed to realise output")?;
+
+                    match &opts.action {
+                        Action::Discover => unreachable!(),
+                        Action::Realise => {}
+                        Action::Switch => {
+                            std::process::Command::new("run0")
+                                .arg("nixos-rebuild")
+                                .arg("--no-reexec")
+                                .arg("--store-path")
+                                .arg(&output.output_path)
+                                .arg("switch")
+                                .status()
+                                .context("Failed to switch to realised output")?;
+                        }
+                        Action::Boot => {
+                            std::process::Command::new("run0")
+                                .arg("nixos-rebuild")
+                                .arg("--no-reexec")
+                                .arg("--store-path")
+                                .arg(&output.output_path)
+                                .arg("boot")
+                                .status()
+                                .context("Failed to boot to realised output")?;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    Ok(())
+}
